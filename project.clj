5c256fb16d193a7bb9416914f451e360a6084579
Tom Brennan <tjb1982@gmail.com>
2018-02-02 09:59:53 -0500

    bumping to 3.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject co.nclk/flax "2.3.0"
+(defproject co.nclk/flax "3.0.0-SNAPSHOT"
   :description "Clojure library: YAML as a Clojure dialect."
   :url "https://github.com/tjb1982/flax"
   :license {:name "Eclipse Public License"
