50922b82b4a8ca7efd65057b0e14b7472d95680d
Tom Brennan <tjb1982@gmail.com>
2018-10-05 22:12:04 -0400

    x

--- test/co/nclk/flax/binding_test.clj
+++ test/co/nclk/flax/binding_test.clj
@@ -10,6 +10,8 @@
 
 
 (deftest test-anonymous-functions
+
+
   (testing "Function in map"
     (let [src "
 ~(map:
@@ -19,6 +21,8 @@
 - ~(range: [0,5]
 "         result (yval src)]
       (-> result (= '(0 1 2 3 4)) is)))
+
+
   (testing "Function in map with environment variable as collection"
     (let [src "
 ~(map:
@@ -31,6 +35,7 @@
           result (yval src env)]
       (-> result (= '("x","x","x","x","x")) is)))
 
+
   (testing "Function in map with environment variable with inter-function scope"
     (let [src "
 ~(map:
@@ -40,6 +45,19 @@
 - ~(range: [0,5]
 "
           env {:an-ex "x"}
+          result (yval src env)]
+      (-> result (= '("x","x","x","x","x")) is)))
+
+
+  (testing "Function in map with environment variable as collection"
+    (let [src "
+~(map:
+- ~(fn:
+  - [arg]
+  - ~@arg
+- ~@five-exes
+"
+          env {:five-exes ["x","x","x","x","x"]}
           result (yval src env)]
       (-> result (= '("x","x","x","x","x")) is)))
   )
